ea7db397f1e8a9c9eb4e2326c303ed621ba001cf
Edfang192 <70400798+Edfang192@users.noreply.github.com>
2023-01-18 20:48:27 -0500

    Add files via upload

--- pythonhw/hw1.py
+++ pythonhw/hw1.py
@@ -1,26 +1,66 @@
 # use spaces. Do NOT use tabs.
-
+'''
+mystring = "Spring\tSemester\t2023 began this\tweek"
+mystring = "a\tthe\t the \tthe\tthen"
+myarray = [("A",2), ("B",5), ("A",2), ("A", 10)]
+myarray = [1, 1.5, 3, 4]
+myarray = [('c', 2), ('a', 3), ('d', 3), ('a', 2), ('e',1), ('c',1)]
+'''
 def q1(mystring):
-    pass
+    newstring = tuple(mystring.split('\t')[1:3])
+    return newstring
+
 
 
 def q2(mystring):
-    pass
+    word = 'the'
+    newstring = mystring.split('\t')
+    numthe = newstring.count('the')
+    return numthe
 
 
 
 def q3(myarray):
-    pass
+    result = []
+    for i in myarray:
+        if i[0] == 'a':
+            result.append(i[1])
+    return sum(result)
 
 
 def q4(myarray):
     """ 
     you cannot change how the array is iterated 
     and you cannot use any list operations on myarray """
-
+    mystringarray = myarray
+    count = 0
     for mynum in mystringarray:
-        pass
+        if mynum % 2 == 0:
+            return count
+        count= count+1
+    else:
+        return -1
+        
+
+        
 
 
 def q5(myarray):
-    pass
+    mydic= {}
+    for letter, num in myarray:
+        if letter not in mydic: mydic[letter] = num
+        else: mydic[letter] = mydic[letter] + num
+    return mydic
+'''
+def main():
+    #print(q1(mystring))
+    #print(q2(mystring))
+    #print(q3(myarray))
+    #print(q4(myarray))
+    #print(q5(myarray))
+    return
+    
+
+if __name__ == '__main__':
+    main()
+'''
