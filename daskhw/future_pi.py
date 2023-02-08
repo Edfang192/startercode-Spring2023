3e5f6a0bad6e4ae92504e9f78e34d79a13385ef7
Edfang192 <70400798+Edfang192@users.noreply.github.com>
2023-02-07 21:59:34 -0500

    Add files via upload

--- daskhw/future_pi.py
+++ daskhw/future_pi.py
@@ -1,5 +1,35 @@
+import dask
+import random
+from dask.distributed import Client
+import time
 
+def worker_batched_job(num):
+    tot = 0
+    for i in range(num):
+        #Samples x and y cords.
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x*x + y*y <= 1:
+            tot += 1
+    return tot
 
 def pi(n):
-    """ runs n simulations to approximate pi """
-    return None
+    with Client(n_workers=4) as c: #set workers to n
+        darts = int(n/4) #divide the simulations by workers to find darts.
+        worker_results = c.map(worker_batched_job, [darts]*4)#Create list of futures.
+        total = c.submit(sum,worker_results)#tot num of hits.
+        start = time.perf_counter()
+        pi = total.result()* 4 / n
+        end = time.perf_counter()
+        print(f"pi estimate: {pi}, in time {end-start}")
+
+    
+'''
+def main():
+    print(pi(5000))
+    return
+    
+
+if __name__ == '__main__':
+    main()
+'''
