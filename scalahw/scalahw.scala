514eda9243fa8cc75d43cd36b36e78ac6ab79dbc
Edfang192 <70400798+Edfang192@users.noreply.github.com>
2023-03-28 20:06:05 -0400

    Update scalahw.scala

--- scalahw/scalahw.scala
+++ scalahw/scalahw.scala
@@ -38,7 +38,25 @@ object HW {
       //do not use return statements.
       f(f(x, y), z)
    }
-
+   def q5_stringy(start: Int, n: Int): Vector[String] = {
+      //Creates a vector with n elements and the two arguments in tabulate maps the indices.
+      
+      Vector.tabulate(n)(i => (start + i).toString)
+   }
+   def q6_modab(a: Int, b: Int, c: Vector[Int]): Vector[Int] = {
+      //Use filter to take elements from c and return a value if the elem is greater than or equal to a.
+      c.filter(element => element >= a && elem % b != 0)
+   }
+   def q7_count(arr: Vector[Int], f: Int => Boolean): Int = {
+      //check if array is empty
+      if (arr.isEmpty) 0
+      else {
+         val countRest = q7_count(arr.tail, f)//recursively call itself to store results in count and adds 1 to the result of Count.
+         //If it does not satisfy, return count
+         if (f(arr.head)) count + 1
+         else count
+      }
+   }
    // create the rest of the functions yourself
    // in order for the code to compile, you need to (at the very least) create
    // blank versions of the remaining functions and have them return a value of 
